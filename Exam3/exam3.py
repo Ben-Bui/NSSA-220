4764e5b7d4e2896eb1642d92e86eb56a934a63bc
Ben Bui <btb4516@g.rit.edu>
2022-12-12 00:31:03 -0500

    update

--- Exam3/exam3.py
+++ Exam3/exam3.py
@@ -1,21 +1,44 @@
+from scapy.all import*
 
-from filter_packets import *
-from packet_parser import *
-from compute_metrics import *
+packets = rdpcap('TeddyBallgame.pcap')
+packets[ICMP].summary()
+
+def compute_metric(ip,L,fn):
+    request_sent_count=0
+    request_received_count=0
+   
+    request_byte_sent_count=0
+    request_byte_received_count=0
+    
+    request_data_sent_count=0
+    request_data_received_count=0
+
+    for item in L :
+            if item[8] == "request":
+                        if item[2] == ip :
+                            request_sent_count += 1
+                            request_byte_sent_count += int(item[5])
+                            request_data_sent_count += int(item[5]) - 42    
+
+                        elif item[3] == ip :
+                            request_received_count += 1
+                            request_byte_received_count += int(item[5])
+                            request_data_received_count += int(item[5]) -42 
+                            
+            elif item[3] == ip :
+                        request_received_count += 1 
+                        request_byte_received_count += int(item[5])
+                        request_data_received_count += int(item[5]) - 42 
+            return(request_sent_count,request_received_count,round(request_byte_sent_count,2),round(request_data_sent_count,2),round(request_byte_received_count,2),round(request_data_received_count,2))
 
 L1 = []
 
 nl="\n"
 
-filter()
 
-parse("TeddyBallgame_filtered.txt", L1)
-
-filename="output.csv"
-
-(a,b,c,d,e,f,g,h,i,j,k,l,m) = compute("192.168.100.2",L1,"TeddyBallgame_filtered.txt")
+(a,b,c,d,e,f,g,h,i,j,k,l,m) = compute_metric("192.168.100.2",L1,"TeddyBallgame")
 file = open(filename,"w")
-file.write("Node1")
+file.write("TeddyBallgame")
 file.write(nl)
 file.write("Echo Requests Sent,Echo Requests Recieved,Echo Replies Sent,Echo Replies Recieved"+ nl )
 file.write(str(a)+","+str(b)+","+str(c)+","+str(d)+nl)
